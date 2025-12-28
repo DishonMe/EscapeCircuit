2f98d0ef72bb238b906b02380db365476d97212d
yuval4604 <yuvalzar@post.bgu.ac.il>
2025-12-29 01:36:00 +0200

    Revert "domainLayer"
    This reverts commit ec41c63836d81a9549003c084c1a04fed1371990.

--- src/Backend/DomainLayer/Exceptions.py
+++
@@ -1,8 +0,0 @@
-class DomainError(Exception):
-    """Base class for domain-level errors."""
-    pass
-
-
-class ValidationError(DomainError):
-    """Raised when domain invariants are violated."""
-    pass
