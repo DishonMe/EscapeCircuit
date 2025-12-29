b3b1de5c14d681f430296bd5b3557adbd7408f6e
yuval4604 <yuvalzar@post.bgu.ac.il>
2025-12-29 16:42:01 +0200

    remove auti generated files from testings

--- src/Backend/DomainLayer/Circuit.py
+++ src/Backend/DomainLayer/Circuit.py
@@ -85,10 +85,10 @@ class Circuit:
         )
 
     # --- getters ---
-    def get_id(self) -> int:
+    def get_id(self) -> str:
         return self.id
 
-    def get_user_id(self) -> int:
+    def get_user_id(self) -> str:
         return self.user_id
 
     def get_name(self) -> str:
